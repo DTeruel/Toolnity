c4050a1bea63a05f7220ef7fe822462195210dd6
DTeruel <david.teruel@gmail.com>
2021-08-30 14:22:26 +0200

    Adding AutoRotate script

---
+++ Runtime/Autos/AutoRotate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Toolnity
+{
+	public class AutoRotate : AutoBaseClass
+	{
+		[SerializeField] private Vector3 vectorToRotate = Vector3.zero;
+		[SerializeField] private float speed = 1.0f;
+
+		
+		protected override void InitInternal()
+		{
+		}
+
+		protected override void PlayInternal()
+		{
+		}
+
+		protected override void StopInternal()
+		{
+		}
+		
+		protected override void UpdateInternal()
+
+		{
+			transform.Rotate(vectorToRotate, speed * Time.deltaTime);
+		}
+	}
+}
