d5c25061fd37d56ad1992a4dc0e28542aaebe43d
david.teruel <david.teruel@gmail.com>
2022-10-25 20:27:05 +0200

    [Shortcuts][v2.0.1] The config file wasn't serialized

--- Packages/Toolnity - Shortcuts/Editor/Scripts/TeleportConfig.cs
+++ Packages/Toolnity - Shortcuts/Editor/Scripts/TeleportConfig.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace Toolnity.Shortcuts
@@ -5,8 +6,46 @@ namespace Toolnity.Shortcuts
 	[CreateAssetMenu(menuName = "Toolnity/Teleport Config", fileName = "Teleport Config")]
 	public class TeleportConfig : ScriptableObject
 	{
-		public bool CopyAxisX = true;
-		public bool CopyAxisY = true;
-		public bool CopyAxisZ = true;
+		[SerializeField]
+		private bool copyAxisX = true;
+		public bool CopyAxisX
+		{
+			get => copyAxisX;
+			set
+			{
+				copyAxisX = value;
+#if UNITY_EDITOR
+				EditorUtility.SetDirty(this);
+#endif
+			}
+		}
+		
+		[SerializeField]
+		private bool copyAxisY = true;
+		public bool CopyAxisY
+		{
+			get => copyAxisY;
+			set
+			{
+				copyAxisY = value;
+#if UNITY_EDITOR
+				EditorUtility.SetDirty(this);
+#endif
+			}
+		}
+		
+		[SerializeField]
+		private bool copyAxisZ = true;
+		public bool CopyAxisZ
+		{
+			get => copyAxisZ;
+			set
+			{
+				copyAxisZ = value;
+#if UNITY_EDITOR
+				EditorUtility.SetDirty(this);
+#endif
+			}
+		}
 	}
 }
